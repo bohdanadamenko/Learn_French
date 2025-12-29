4b7cf8a9479dd636130ff539f3f9e1b78e4735b2
AdamenkoBohdan <adamenko.bogdan@gmail.com>
2025-12-29 21:22:44 +0100

    Remove legacy_lessons directory

--- legacy_lessons/urls.py
+++
@@ -1,9 +0,0 @@
-# lessons/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Этот маршрут правильный. Он вызывает views.index.
-    path('', views.index, name='index'),
-]
