bcb774b0e7b4e5a57e43463de9431a0e8e075d16
AdamenkoBohdan <adamenko.bogdan@gmail.com>
2026-01-09 10:01:51 +0100

    fix: move google oauth env vars to CI workflow

--- learn_french/settings.py
+++ learn_french/settings.py
@@ -289,8 +289,8 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APP': {
-            'client_id': config('GOOGLE_CLIENT_ID', default=''),
-            'secret': config('GOOGLE_CLIENT_SECRET', default=''),
+            'client_id': config('GOOGLE_CLIENT_ID'),
+            'secret': config('GOOGLE_CLIENT_SECRET'),
             'key': ''
         },
         'SCOPE': [
