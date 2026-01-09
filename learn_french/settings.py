07b819b3d1f10e359900a7a31c563333beb6286c
AdamenkoBohdan <adamenko.bogdan@gmail.com>
2026-01-09 08:26:04 +0100

    feat: set default language to French

--- learn_french/settings.py
+++ learn_french/settings.py
@@ -208,7 +208,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'fr'
 
 LANGUAGES = [
     ('ru', 'Russian'),
